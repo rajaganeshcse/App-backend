1a3740bd88294b164bd25914a11587196109a1d2
Rajaganesh <ganesh209832@gmail.com>
2026-04-26 15:16:45 +0530

    Update withdrawService.java

--- src/main/java/com/example/backend/service/withdrawService.java
+++ src/main/java/com/example/backend/service/withdrawService.java
@@ -68,7 +68,7 @@ public class withdrawService {
 
             // ✅ Create withdraw request
             DocumentReference reqRef =
-                    db.collection("redeem_requests").document(uid);
+                    db.collection("redeem_requests").document();
 
             String requestId = reqRef.getId();
 
