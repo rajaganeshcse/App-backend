b7105367349e5276a540387ac907a840440af6c8
ganesh <ganesh209832@gmail.com>
2026-06-08 14:53:27 +0530

    strike10

--- src/main/java/com/example/backend/controller/live.java
+++ src/main/java/com/example/backend/controller/live.java
@@ -1,18 +1,26 @@
 package com.example.backend.controller;
 
 
+import com.google.firebase.FirebaseApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.PublicKey;
+
 @RestController
 @RequestMapping("/live")
 public class live {
+    @Autowired
+    private FirebaseApp firebaseApp;
 
-    @GetMapping("/live")
+    @GetMapping("/1")
     public String live() {
-
         return "live";
-
+    }
+    @GetMapping("temp/reedem")
+    public String reedem(){
+        return "Ganesh";
     }
 }
